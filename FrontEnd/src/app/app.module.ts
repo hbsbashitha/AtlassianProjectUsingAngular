e27a4ee0e4e16d3bb704057a7466124df7b073fa
Bhashitha Ranasinghe <Bhashitha@ebuilder.local>
2023-01-03 09:17:50 +0530

    add popup section

--- FrontEnd/src/app/app.module.ts
+++ FrontEnd/src/app/app.module.ts
@@ -1,20 +1,28 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginPageComponent } from './components/login-page/login-page.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { Error404PageComponent } from './components/error404-page/error404-page.component';
+import { PopupModelComponent } from './components/popup-model/popup-model.component';
+import { UserDetailsPageComponent } from './components/user-details-page/user-details-page.component';
+// import { MdbCheckboxModule } from 'mdb-angular-ui-kit/checkbox';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginPageComponent,
-    NavbarComponent
+    NavbarComponent,
+    Error404PageComponent,
+    PopupModelComponent,
+    UserDetailsPageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    // MdbCheckboxModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
